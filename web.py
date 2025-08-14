b8e2d9b2fa7b5610690f33c3b061b248b987569f
ramin <raminblues2003@gmail.com>
2025-08-14 12:30:53 +0330

    A simple to-do list web app

---
+++ web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from streamlit import checkbox
+
+import functions
+
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+
+st.title("Todo App")
+st.subheader("")
+st.write("")
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
+
+
